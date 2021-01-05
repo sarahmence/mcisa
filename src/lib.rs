931706e8bbee0b087888a4898d8db05a0c0a59d2
theycouldbedeloi <theycouldbedeloi@gmail.com>
2021-01-05 17:43:29 -0600

    Added library header

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/*
+ * lib.rs
+ * Main library header for mcisa
+ * Created by Deloi Davis
+ * Created on 1/5/2021
+ *
+ * Copyright (C) 2021 Deloi Davis
+ *
+ * Licensed under the MIT License (see LICENSE file for details)
+ */
+
+// TODO: Add module exports
+
+// end of file
