8371d6ff4273ee237051af3048adfb286320e540
theycouldbedeloi <theycouldbedeloi@gmail.com>
2021-01-05 21:57:40 -0600

    Added cpu module

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,6 +1,6 @@
 /*
- * lib.rs
- * Main library header for mcisa
+ * mod.rs
+ * Module header for mcisa's CPU module
  * Created by Deloi Davis
  * Created on 1/5/2021
  *
@@ -9,7 +9,7 @@
  * Licensed under the MIT License (see LICENSE file for details)
  */
 
-// module exports
-pub mod cpu;
+// TODO: add module exports
+
 
 // end of file
